d4ba0d5253dddc33eb05d7a4b4ec23f818c75af6
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2017-08-16 13:51:24 -0300

    File loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,13 @@ module.exports = {
     output: {
         filename: "bundle.js",
         path: path.join(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpe?g$/,
+                use: [ "file-loader" ]
+            }
+        ]
     }
 }
